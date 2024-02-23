2820bf62f42506ec5ca3dd469b5a1efb2e169d90
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2024-02-23 19:09:31 +0530

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='p-4'>
+      <h1 className='text-center font-bold text-5xl bg-gradient-to-r from-teal-500 via-indigo-500 to-green-500 bg-clip-text text-transparent pb-4'>Our IITian Team</h1>
+      <div className='text-center text-xl text-white pt-4'>A Team Of IITians to Boost your Tech Career to New Heights. The Curriculum Team is an experienced group of data analyst instructors</div>
+    </div>
+  )
+}
+
+export default App
